c1620ae2de03a7d9bc87619456496d3c16aef134
tgtiburon <tg.tiburon@gmail.com>
2022-03-25 13:38:10 -0400

    changed wording on about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import coverImage from "../../assets/bubble_framed_pike_pic.png";
 
-
 function About() {
   // Return about HTML
   return (
@@ -15,10 +14,11 @@ function About() {
         width="256"
       />
       <p>
-        Highly effective educator with more than 25 years of experience teaching
-        science and developing meaningful relationships with students. Tenacious
-        problem solver and creative thinker who will help you reach your
-        full-stack web development goals.
+        Tenacious problem solver and creative thinker who will help you reach
+        your full-stack web development goals. Even find joy in debugging other
+        people’s code. Formerly a highly effective educator with more than 25
+        years of experience teaching science and developing meaningful
+        relationships with students.
       </p>
     </section>
   );
